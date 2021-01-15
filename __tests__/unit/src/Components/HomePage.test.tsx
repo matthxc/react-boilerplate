2d9988f0d2f6544c1e5595fb5e6154426910b86f
matthxc <juan9505@gmail.com>
2021-01-14 23:05:02 -0500

    build: update dependencies

--- __tests__/unit/src/Components/HomePage.test.tsx
+++ __tests__/unit/src/Components/HomePage.test.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import { HomePage } from '@Pages/HomePage';
 
 describe('<HomePage /> component tests', () => {
   it('should render correctly', () => {
-    render(<HomePage />);
+    render(
+      <BrowserRouter>
+        <HomePage />
+      </BrowserRouter>,
+    );
 
     expect(screen.getByText('Home page')).toBeInTheDocument();
     expect(screen.getByText('Hello world')).toBeInTheDocument();
